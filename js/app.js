60244ac9858710ef5c5954129805e6571af90d49
Tushar <Tushar@Tushars-MacBook-Pro.local>
2015-02-10 13:55:24 +0530

    Go Go Go !

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -20,60 +20,38 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     });
 })
 
-.config(function ($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
 
     $stateProvider
-        .state('app', {
-            url: "/app",
-            abstract: true,
-            templateUrl: "templates/menu.html",
-            controller: 'AppCtrl'
-        })
 
-    .state('app.search', {
-        url: "/search",
+    .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'AppCtrl'
+    })
+
+    /*Sidemenu Pages*/
+    
+    .state('app.home', {
+        url: "/home",
         views: {
             'menuContent': {
-                templateUrl: "templates/search.html"
+                templateUrl: "templates/home.html",
+                controller: 'HomeCtrl'
             }
         }
     })
+    
+    /*Simple Pages*/
 
-    .state('app.browse', {
-            url: "/browse",
-            views: {
-                'menuContent': {
-                    templateUrl: "templates/browse.html"
-                }
-            }
-        })
-    .state('app.login', {
-            url: "/login",
-            views: {
-                'menuContent': {
-                    templateUrl: "templates/loginpage.html"
-                }
-            }
-        })
-        .state('app.playlists', {
-            url: "/playlists",
-            views: {
-                'menuContent': {
-                    templateUrl: "templates/playlists.html",
-                    controller: 'PlaylistsCtrl'
-                }
-            }
-        })
+    .state('login', {
+        url: "/login",
+        templateUrl: "templates/login.html",
+        controller: 'LoginCtrl'
+    })
 
-    .state('app.single', {
-        url: "/playlists/:playlistId",
-        views: {
-            'menuContent': {
-                templateUrl: "templates/playlist.html",
-                controller: 'PlaylistCtrl'
-            }
-        }
-    });
+    ;
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/playlists');
+    $urlRouterProvider.otherwise('/app/home');
 });
