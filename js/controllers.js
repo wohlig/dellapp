03f7037c3980e99d3c005e19c915b514d4aa7151
Chintan Shah <chintan@wohlig.com>
2015-03-05 18:17:11 +0530

    other

--- js/controllers.js
+++ js/controllers.js
@@ -1,129 +1,127 @@
-angular.module('starter.controllers', ['ionic','templateservicemod','myservices'])
+angular.module('starter.controllers', ['ionic', 'templateservicemod', 'myservices'])
 
-.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {})
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {})
 
-.controller('LogoutCtrl', function ($scope, $ionicModal, $timeout, MyServices) {
-    
-    var logoutsuccess = function (data, status){
+.controller('LogoutCtrl', function($scope, $ionicModal, $timeout, MyServices) {
+
+    var logoutsuccess = function(data, status) {
         console.log(data);
     }
     MyServices.logout().success(logoutsuccess);
-    
+
 })
 
-.controller('HomeCtrl', function ($scope, $stateParams, TemplateService, MyServices, $location) {
+.controller('HomeCtrl', function($scope, $stateParams, TemplateService, MyServices, $location) {
     TemplateService.noactive();
     TemplateService.homeclass = "active";
     $scope.user = [];
-    
-    
+
+
     //  AUTHENTICATE
-    var usersuccess = function (data, status) {
+    var usersuccess = function(data, status) {
         console.log(data);
         $scope.user = data;
     }
-    var authenticatesuccess = function (data, status) {
-        if(data=="false")
-        {
+    var authenticatesuccess = function(data, status) {
+        if (data == "false") {
             $location.url("/login");
-        }else{
+        } else {
             MyServices.getuser(data).success(usersuccess);
         }
     }
     MyServices.authenticate().success(authenticatesuccess);
-    
-    $scope.facebooklogin = function() {
-        window.open('http://dellcampassador.com/index.php/hauth/login/Facebook', '_blank', 'location=yes');
-    };
-    // GET USER DATA
-    
+
+
+
 })
 
-.controller('LoginCtrl', function ($scope, $stateParams, MyServices, $location, $ionicPopup,$ionicSlideBoxDelegate) {
+.controller('LoginCtrl', function($scope, $stateParams, MyServices, $location, $ionicPopup, $ionicSlideBoxDelegate) {
 
     //  AUTHENTICATE
-    var authenticatesuccess = function (data, status) {
+    var authenticatesuccess = function(data, status) {
         console.log(data);
-        if(data!="false")
-        {
+        if (data != "false") {
             $location.url("app/home");
         }
     }
     MyServices.authenticate().success(authenticatesuccess);
-    
-    
-    $scope.changetologin=function() {
+
+
+    $scope.changetologin = function() {
         $ionicSlideBoxDelegate.slide(1);
     };
-    
+
     //  ON LOGIN CLICK
-    var loginsuccess = function (data, status) {
+    var loginsuccess = function(data, status) {
         console.log(data);
-        if(data=="false"){
+        if (data == "false") {
             var alertPopup = $ionicPopup.alert({
                 title: 'Login',
                 template: 'Invalide Username Or Password'
             });
-        }else{
+        } else {
             $location.url("app/home");
         }
     }
-    
-    $scope.normallogin = function (login){
+
+    $scope.normallogin = function(login) {
         MyServices.normallogin(login).success(loginsuccess);
     }
+    $scope.facebooklogin = function() {
+        window.open('http://dellcampassador.com/index.php/hauth/login/Facebook', '_blank', 'location=yes');
+    };
+    // GET USER DATA
 
 })
 
-.controller('WelcomeCtrl', function ($scope, $stateParams, $ionicModal) {
+.controller('WelcomeCtrl', function($scope, $stateParams, $ionicModal) {
 
     $ionicModal.fromTemplateUrl('templates/model.html', {
         scope: $scope,
         animation: 'slide-in-up'
-    }).then(function (modal) {
+    }).then(function(modal) {
         $scope.modal = modal;
     });
 
-    $scope.openedit = function () {
+    $scope.openedit = function() {
         $scope.modal.show();
     }
 
-    $scope.closeModal = function () {
+    $scope.closeModal = function() {
         $scope.modal.hide();
     };
 
 })
 
-.controller('ProfileCtrl', function ($scope, $stateParams, $ionicModal, TemplateService, MyServices, $location, $ionicPopup) {
+.controller('ProfileCtrl', function($scope, $stateParams, $ionicModal, TemplateService, MyServices, $location, $ionicPopup) {
     TemplateService.noactive();
     TemplateService.homeclass = "active";
-    
-    
+
+
     $scope.user = [];
     $scope.profile = [];
-    
+
     //  AUTHENTICATE
-    var usersuccess = function (data, status) {
+    var usersuccess = function(data, status) {
         console.log(data);
         $scope.user = data;
     }
-	var profilesuccess = function (data, status) {
-		console.log(data);
-		$scope.profile = data;
-	}
-    var authenticatesuccess = function (data, status) {
-        if(data=="false")
-        {
+    var profilesuccess = function(data, status) {
+        console.log(data);
+        $scope.profile = data;
+    }
+    var authenticatesuccess = function(data, status) {
+        if (data == "false") {
             $location.url("/login");
-        }else{
+        } else {
             MyServices.getuser(data).success(usersuccess);
-			MyServices.editprofilebefore().success(profilesuccess);
+            MyServices.editprofilebefore().success(profilesuccess);
         }
     }
     MyServices.authenticate().success(authenticatesuccess);
-    
-    
-    $scope.mkopen = function () {
+
+
+    $scope.mkopen = function() {
         console.log("cbd");
         if ($scope.shldopens == "openprf") {
             $scope.shldopens = "";
@@ -135,18 +133,18 @@ angular.module('starter.controllers', ['ionic','templateservicemod','myservices'
     $ionicModal.fromTemplateUrl('templates/editprofile.html', {
         scope: $scope,
         animation: 'slide-in-up'
-    }).then(function (modal) {
+    }).then(function(modal) {
         $scope.modal = modal;
     });
 
-    $scope.openedit = function () {
+    $scope.openedit = function() {
         $scope.modal.show();
     }
 
-    $scope.closeModal = function () {
+    $scope.closeModal = function() {
         $scope.modal.hide();
     };
-    $scope.mkopen1 = function () {
+    $scope.mkopen1 = function() {
         console.log("cbd");
         if ($scope.shldopens1 == "changesul" &&
             $scope.shldopens2 == "changesi") {
@@ -158,7 +156,7 @@ angular.module('starter.controllers', ['ionic','templateservicemod','myservices'
         }
     };
 
-    $scope.mkopen2 = function () {
+    $scope.mkopen2 = function() {
         console.log("cbd");
         if ($scope.shldopens3 == "changesul1" &&
             $scope.shldopens4 == "changesi") {
@@ -170,233 +168,229 @@ angular.module('starter.controllers', ['ionic','templateservicemod','myservices'
         }
     };
 
-	$scope.savepassword = function () {
-		$scope.shldopens3 = "changesul1";
-		$scope.shldopens4 = "changesi";
-	}
-	
+    $scope.savepassword = function() {
+        $scope.shldopens3 = "changesul1";
+        $scope.shldopens4 = "changesi";
+    }
+
     TemplateService.noactive();
     TemplateService.profileclass = "active";
-	
-	
-	// CHANGE PASSWORD
-	
-	var passwordsuccess = function (data, status) {
-		console.log(data);
-		if(data=="false"){
-			var alertPopup = $ionicPopup.alert({
+
+
+    // CHANGE PASSWORD
+
+    var passwordsuccess = function(data, status) {
+        console.log(data);
+        if (data == "false") {
+            var alertPopup = $ionicPopup.alert({
                 title: 'Change Password',
                 template: 'Error In Update'
             });
-		}else{
-			var alertPopup = $ionicPopup.alert({
+        } else {
+            var alertPopup = $ionicPopup.alert({
                 title: 'Change Password',
                 template: 'Password Updated Successfully'
             });
-		}
-	}
-	$scope.savepassword = function (password) {
-		console.log(password);
-		MyServices.changepassword(password).success(passwordsuccess);
-	}
-	
-	// CHANGE PROFILE
-	
-	var changeprofilesuccess = function (data, status) {
-		console.log(data);
-		if(data==false){
-			var alertPopup = $ionicPopup.alert({
+        }
+    }
+    $scope.savepassword = function(password) {
+        console.log(password);
+        MyServices.changepassword(password).success(passwordsuccess);
+    }
+
+    // CHANGE PROFILE
+
+    var changeprofilesuccess = function(data, status) {
+        console.log(data);
+        if (data == false) {
+            var alertPopup = $ionicPopup.alert({
                 title: 'Change Profile',
                 template: 'Error In Update'
             });
-		}else{
-			var alertPopup = $ionicPopup.alert({
+        } else {
+            var alertPopup = $ionicPopup.alert({
                 title: 'Change Profile',
                 template: 'Profile Updated successfully'
             });
-		}
-	}
-	$scope.saveprofile = function (profile) {
-		console.log(profile);
-		MyServices.changeprofile(profile).success(changeprofilesuccess);
-	}
-	
+        }
+    }
+    $scope.saveprofile = function(profile) {
+        console.log(profile);
+        MyServices.changeprofile(profile).success(changeprofilesuccess);
+    }
+
 })
 
-.controller('EditprofileCtrl', function ($scope, $stateParams, MyServices, $location) {
+.controller('EditprofileCtrl', function($scope, $stateParams, MyServices, $location) {
 
-    
+
     $scope.user = 0;
-    
-    
+
+
     //  AUTHENTICATE
-    var authenticatesuccess = function (data, status) {
-        if(data=="false")
-        {
+    var authenticatesuccess = function(data, status) {
+        if (data == "false") {
             $location.url("/login");
-        }else{
-			$scope.user = data;
-		}
+        } else {
+            $scope.user = data;
+        }
     }
     MyServices.authenticate().success(authenticatesuccess);
-	
-	// CHANGE PASSWORD
-	var passwordsuccess = function (data, status) {
-		console.log(data);
-	}
-	$scope.savepassword = function (password) {
-		console.log(password);
-		MyServices.changepassword(password).success(passwordsuccess);
-	}
+
+    // CHANGE PASSWORD
+    var passwordsuccess = function(data, status) {
+        console.log(data);
+    }
+    $scope.savepassword = function(password) {
+        console.log(password);
+        MyServices.changepassword(password).success(passwordsuccess);
+    }
 
 })
 
-.controller('PostinfoCtrl', function ($scope, $stateParams) {})
+.controller('PostinfoCtrl', function($scope, $stateParams) {})
 
-.controller('SuggestpostCtrl', function ($scope, $stateParams) {})
+.controller('SuggestpostCtrl', function($scope, $stateParams) {})
 
-.controller('LeaderboardCtrl', function ($scope, $stateParams, TemplateService, MyServices, $location, $ionicScrollDelegate) {
+.controller('LeaderboardCtrl', function($scope, $stateParams, TemplateService, MyServices, $location, $ionicScrollDelegate) {
     TemplateService.noactive();
     TemplateService.leaderclass = "active";
-	
+
     $scope.leaderboard = [];
-	$scope.rankuser = [];
-	$scope.user = [];
-	$scope.pageno = 1;
-	$scope.totallength = 0;
-    
-    
+    $scope.rankuser = [];
+    $scope.user = [];
+    $scope.pageno = 1;
+    $scope.totallength = 0;
+
+
     //  AUTHENTICATE
-	var usersuccess = function (data, status) {
-		$scope.user = data;
-	}
-    var leaderboardsuccess = function (data, status) {
-		$scope.totallength = data.totalvalues-3;
-		$scope.rankuser = data.queryresult.slice(0,3);
-        $scope.leaderboard = data.queryresult.splice(3);
-		$scope.$broadcast('scroll.infiniteScrollComplete');
+    var usersuccess = function(data, status) {
+        $scope.user = data;
     }
-	var leaderboardsuccesspush = function (data, status) {
-		for (var i = 0; i < data.queryresult.length; i++) {
+    var leaderboardsuccess = function(data, status) {
+        $scope.totallength = data.totalvalues - 3;
+        $scope.rankuser = data.queryresult.slice(0, 3);
+        $scope.leaderboard = data.queryresult.splice(3);
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+    }
+    var leaderboardsuccesspush = function(data, status) {
+        for (var i = 0; i < data.queryresult.length; i++) {
             $scope.leaderboard.push(data.queryresult[i]);
         }
-		$scope.$broadcast('scroll.infiniteScrollComplete');
-	}
-    var authenticatesuccess = function (data, status) {
-        if(data=="false")
-        {
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+    }
+    var authenticatesuccess = function(data, status) {
+        if (data == "false") {
             $location.url("/login");
-        }else{
+        } else {
             MyServices.getleaderboard($scope.pageno).success(leaderboardsuccess);
-			MyServices.getuser(data).success(usersuccess);
+            MyServices.getuser(data).success(usersuccess);
         }
     }
     MyServices.authenticate().success(authenticatesuccess);
-	
-	$scope.loadMore = function () {
-		console.log("loading.....");
-		console.log($scope.leaderboard.length);
-		console.log($scope.totallength);
-        
-		
+
+    $scope.loadMore = function() {
+        console.log("loading.....");
+        console.log($scope.leaderboard.length);
+        console.log($scope.totallength);
+
+
         if ($scope.leaderboard.length != $scope.totallength) {
             $scope.pageno = $scope.pageno + 1;
             MyServices.getleaderboard($scope.pageno).success(leaderboardsuccesspush);
         }
-		
-	}
+
+    }
 
 })
 
-.controller('PostCtrl', function ($scope, $stateParams) {})
+.controller('PostCtrl', function($scope, $stateParams) {})
 
-.controller('CreatepostCtrl', function ($scope, $stateParams) {})
+.controller('CreatepostCtrl', function($scope, $stateParams) {})
 
-.controller('PostfbCtrl', function ($scope, $stateParams) {})
+.controller('PostfbCtrl', function($scope, $stateParams) {})
 
-.controller('PostinfofbCtrl', function ($scope, $stateParams) {})
+.controller('PostinfofbCtrl', function($scope, $stateParams) {})
 
-.controller('SuggestedpostCtrl', function ($scope, TemplateService) {
+.controller('SuggestedpostCtrl', function($scope, TemplateService) {
     TemplateService.noactive();
     TemplateService.createpostclass = "active";
 })
 
-.controller('TableCtrl', function ($scope, $stateParams, TemplateService, MyServices, $location) {
+.controller('TableCtrl', function($scope, $stateParams, TemplateService, MyServices, $location) {
     TemplateService.noactive();
     TemplateService.tabletwitclass = "active";
-	
+
     $scope.user = 0;
-	$scope.twitter = [];
-    
+    $scope.twitter = [];
+
     //  AUTHENTICATE
-	var twittersuccess = function (data, status) {
-		console.log(data);
-		$scope.twitter = data;
-	}
-    var authenticatesuccess = function (data, status) {
-        if(data=="false")
-        {
+    var twittersuccess = function(data, status) {
+        console.log(data);
+        $scope.twitter = data;
+    }
+    var authenticatesuccess = function(data, status) {
+        if (data == "false") {
             $location.url("/login");
-        }else{
-			$scope.user = data;
-			MyServices.gettwitterposts().success(twittersuccess);
-		}
+        } else {
+            $scope.user = data;
+            MyServices.gettwitterposts().success(twittersuccess);
+        }
     }
     MyServices.authenticate().success(authenticatesuccess);
 })
 
-.controller('TablefbCtrl', function ($scope, $stateParams, TemplateService, MyServices, $location) {
+.controller('TablefbCtrl', function($scope, $stateParams, TemplateService, MyServices, $location) {
     TemplateService.noactive();
     TemplateService.tablefbclass = "active";
-	
+
     $scope.user = 0;
-	$scope.facebook = [];
-    
+    $scope.facebook = [];
+
     //  AUTHENTICATE
-	var facebooksuccess = function (data, status) {
-		console.log(data);
-		$scope.facebook = data;
-	}
-    var authenticatesuccess = function (data, status) {
-        if(data=="false")
-        {
+    var facebooksuccess = function(data, status) {
+        console.log(data);
+        $scope.facebook = data;
+    }
+    var authenticatesuccess = function(data, status) {
+        if (data == "false") {
             $location.url("/login");
-        }else{
-			$scope.user = data;
-			MyServices.getfacebookposts().success(facebooksuccess);
-		}
+        } else {
+            $scope.user = data;
+            MyServices.getfacebookposts().success(facebooksuccess);
+        }
     }
     MyServices.authenticate().success(authenticatesuccess);
-	
+
 })
 
-.controller('TwitterpostCtrl', function ($scope, $stateParams, TemplateService) {
+.controller('TwitterpostCtrl', function($scope, $stateParams, TemplateService) {
     TemplateService.noactive();
     TemplateService.twitpostclass = "active";
 
 })
 
-.controller('FbpostCtrl', function ($scope, $stateParams, TemplateService) {
+.controller('FbpostCtrl', function($scope, $stateParams, TemplateService) {
     TemplateService.noactive();
     TemplateService.fbpostclass = "active";
 })
 
-.controller('MenuCtrl', function ($scope, $stateParams, TemplateService, MyServices, $location) {
-    
+.controller('MenuCtrl', function($scope, $stateParams, TemplateService, MyServices, $location) {
+
     // DEVELOPMENT
     var logoutsuccess = function(data, status) {
         console.log(data);
         $location.url("/login");
     }
-    $scope.logoutapp = function () {
-        
+    $scope.logoutapp = function() {
+
         MyServices.logout().success(logoutsuccess);
-        
+
     }
-    
-    
+
+
     // DESIGN
-    $scope.changeopen1 = function () {
+    $scope.changeopen1 = function() {
         if ($scope.shouldopen1 == "open") {
             $scope.shouldopen1 = "";
         } else {
@@ -406,7 +400,7 @@ angular.module('starter.controllers', ['ionic','templateservicemod','myservices'
 
     $scope.navigation = TemplateService;
 
-    $scope.changeopen2 = function () {
+    $scope.changeopen2 = function() {
         if ($scope.shouldopen2 == "open") {
             $scope.shouldopen2 = "";
         } else {
