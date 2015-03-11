fe8af2a1ebf43d07e660ed092c644e5d37c8ce9d
Tushar <tushar@wohlig.com>
2015-03-11 17:33:15 +0530

    Done

--- js/appservice.js
+++ js/appservice.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-//var adminurl = "http://dellcampassador.com/new/index.php/json/";
-=======
-var adminurl = "http://dellcampassador.com/new/index.php/json/";
->>>>>>> origin/master
-var adminurl = "http://localhost/dellbackend1.0/index.php/json/";
+var adminurl = "http://dellcampassador.com/new/index.php/json/"
+//var adminurl = "http://localhost/dellbackend1.0/index.php/json/";
 var myservices = angular.module('myservices', [])
 
 .factory('MyServices', function ($http, $location) {
